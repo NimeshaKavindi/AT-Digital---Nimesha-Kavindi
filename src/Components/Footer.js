00e786c8dc198065ccb8298a0ebb4982135454d1
Nimesha Kavindi <abeykoo-se19021@stu.kln.ac.lk>
2023-12-28 10:42:50 +0530

    Add Our technologies and service

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,32 +7,35 @@ function Footer() {
   return (
     <footer className="footer">
         <div className="container-fluid">
-        <div className="row">
-        <div className="col-6">
-          
-          <div className="logo">
-            <img src ={logo} className="logo"/>
-          </div>
-        </div>
-        <div className="col-6">
-          {/* Right side columns */}
-          <div className="row">
-            <div className="col-4">Column 1</div>
-            <div className="col-4">Column 2</div>
-            <div className="col-4">Column 3</div>
-          </div>
-          <div className="row">
-            <div className="col-4">Column 4</div>
-            <div className="col-4">Column 5</div>
-            <div className="col-4">Column 6</div>
-          </div>
-        </div>
-        </div>
-        <div className="row">
-            <div className="col-12" >
-         
+            <div className="row align-items-start">
+                <div className="col-6">
+                
+                    <div className="logo">
+                        <img src ={logo} className="logo"/>
+                    </div><br/>
+                    <div className="text">
+                        <p>
+                    Your goal is our target. Not anything in between. We use<br/>
+                    online marketing platforms and tools to achieve single<br/>
+                    objective - your business results.
+                    </p>
+                    </div>
+                
+                </div>
+                <div className="col-6">
+
+                    <div className="row technologies-service">
+                        <div className="col-4">Our Technologies</div>
+                        <div className="col-4">Our Service</div>
+                    </div>
+                    
+                </div>
             </div>
-        </div>
+            <div className="row">
+                <div className="col-12" >
+            
+                </div>
+            </div>
         </div>
   </footer>
   );
