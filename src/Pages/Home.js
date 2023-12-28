c99088d7fde8105c5c34a33a787dd048c605933b
Nimesha Kavindi <abeykoo-se19021@stu.kln.ac.lk>
2023-12-28 14:59:12 +0530

    Add a custom box and include the content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react"
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
+import Button from "../Components/Button";
 import hero from "../assests/Hero Image.png";
 import "../styles/home.css"
 
@@ -12,7 +13,13 @@ function Home(){
                <img src ={hero} className="hero-img"/>
             </div>
             <div className="custom-box">
-                {/* Content for the box goes here */}
+                <div className="custom-text">
+                  We Crush Your<br/> Competitors, Goals, And <br/> Sales Records - Without<br/> The B.S.
+                </div> 
+                <div>
+                 <Button/>                   
+                </div>
+
             </div>
 
         <Footer/>
