47e765d3a390bc85ab68d612530cb82b2098079b
Nimesha Kavindi <abeykoo-se19021@stu.kln.ac.lk>
2023-12-28 14:12:49 +0530

    Make button component with css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
+import Button from "./Components/Button";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Button/>
       <Footer />
     </div>
   );
---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/button.css"
+
+function Button(){
+    return(
+    <button className="custom-button">
+      GET FREE CONSULTATION
+    </button>
+    );
+}
+
+export default Button;
