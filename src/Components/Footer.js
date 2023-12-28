4e8933b5dc685dad09e1538213850d3d2210c520
Nimesha Kavindi <abeykoo-se19021@stu.kln.ac.lk>
2023-12-28 10:19:14 +0530

    Add logo and make css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "../styles/footer.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "../assests/Picture1.png"
 
 function Footer() {
   return (
     <footer className="footer">
         <div className="container-fluid">
         <div className="row">
-            <div className="col-12" >
-            
-            </div>
+        <div className="col-6">
+          
+          <div className="logo">
+            <img src ={logo} className="logo"/>
+          </div>
+        </div>
+        <div className="col-6">
+          {/* Right side columns */}
+          <div className="row">
+            <div className="col-4">Column 1</div>
+            <div className="col-4">Column 2</div>
+            <div className="col-4">Column 3</div>
+          </div>
+          <div className="row">
+            <div className="col-4">Column 4</div>
+            <div className="col-4">Column 5</div>
+            <div className="col-4">Column 6</div>
+          </div>
+        </div>
         </div>
         <div className="row">
             <div className="col-12" >
